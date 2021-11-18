c491357fae57f4f9e997d50fa60910db66ca5c8f
codepiano <codepiano.li@gmail.com>
2021-11-18 21:16:27 +0800

    rename type

--- y-bang.go
+++ y-bang.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-type lengthType func(list []interface{}) int
-type L_Type func(length lengthType) lengthType
+type lengthTypeYbang func(list []interface{}) int
+type L_Type func(length lengthTypeYbang) lengthTypeYbang
 
 // 递归版本
 func length1(list []interface{}) int {
@@ -30,7 +30,7 @@ func length2(list []interface{}) int {
 // 赋值版本，x 的初始值是什么无所谓，只要满足引用自身即可
 func length3(list []interface{}) int {
 	// lisp 里面没有变量声明，所以需要定义 (let ((h lambda (l) (quote ())))...)
-	var h lengthType
+	var h lengthTypeYbang
 	// 引用自身
 	h = func(list []interface{}) int {
 		if len(list) == 0 {
@@ -46,7 +46,7 @@ func length3(list []interface{}) int {
 func length4(list []interface{}) int {
 
 	// 接收一个 length 类型的函数，返回包装了 length 的函数
-	L := func(length lengthType) lengthType {
+	L := func(length lengthTypeYbang) lengthTypeYbang {
 		return func(list []interface{}) int {
 			if len(list) == 0 {
 				return 0
@@ -57,7 +57,7 @@ func length4(list []interface{}) int {
 	}
 
 	// lisp 里面没有变量声明，所以需要定义 (let ((h lambda (l) (quote ())))...)
-	var h lengthType
+	var h lengthTypeYbang
 	// 新的 length 函数
 	// h 的值只变化一次，在执行完 L 后，h 的值才被确定，作为 L 参数的函数中的 h 的值也才被确定
 	h = L(func(arg []interface{}) int {
@@ -83,7 +83,7 @@ func length4(list []interface{}) int {
 func length5(list []interface{}) int {
 
 	// 接收一个 length 类型的函数，返回包装了 length 的函数
-	L := func(length lengthType) lengthType {
+	L := func(length lengthTypeYbang) lengthTypeYbang {
 		return func(list []interface{}) int {
 			if len(list) == 0 {
 				return 0
@@ -93,8 +93,8 @@ func length5(list []interface{}) int {
 		}
 	}
 
-	yBang := func(L L_Type) lengthType {
-		var h lengthType
+	yBang := func(L L_Type) lengthTypeYbang {
+		var h lengthTypeYbang
 		h = L(func(list []interface{}) int {
 			return h(list)
 		})
